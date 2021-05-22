6ee519a14c1d670a141ce83c2aae26575a8f8934
Edward Pang <53325250+epang22@users.noreply.github.com>
2021-05-22 15:26:25 -0400

    Update disorientation.m
    fix bugs

--- disorientation.m
+++ disorientation.m
@@ -9,9 +9,12 @@ function [misorient, disorient] = disorientation(euler1, euler2, symID)
 % -misorient: misorientation (deg)
 % -disorient: disorientation (deg)
 % Symmetry element matrices from: http://www.cryst.ehu.es/rep/point.html
-% *have not checked that coordinate systems agree for all symmetries!
-% Requires eu2qu.m
+% *have not checked that coordinate systems agree!
 % 2/7/20 (Edward Pang, MIT)
+% Change log:
+% -8/20/20 ELP: make euler angles double (to prevent singularities),
+% comment out inversion symmetry elements, deal with triclinic symmetry in
+% disorientation calcs
 
 
 % numerical tolerance threshold
@@ -21,30 +24,30 @@ thr = 1e-6;     % to deal with single precision error
 % symmetry operators
 if symID==1     % -1
     R{1} = eye(3);
-    R{2} = -R{1};
+%     R{2} = -R{1};
 elseif symID==20    % 2/m
     R{1} = eye(3);
     R{2} = [-1 0 0;0 1 0;0 0 -1];
-    R{3} = -R{1};
-    R{4} = -R{2};
+%     R{3} = -R{1};
+%     R{4} = -R{2};
 elseif symID==22    % mmm
     R{1} = eye(3);
     R{2} = [-1 0 0;0 -1 0;0 0 1];
     R{3} = [-1 0 0;0 1 0;0 0 -1];
     R{4} = [1 0 0;0 -1 0;0 0 -1];
-    R{5} = -R{1};
-    R{6} = -R{2};
-    R{7} = -R{3};
-    R{8} = -R{4};
+%     R{5} = -R{1};
+%     R{6} = -R{2};
+%     R{7} = -R{3};
+%     R{8} = -R{4};
 elseif symID==4     % 4/m
     R{1} = eye(3);
     R{2} = [-1 0 0;0 -1 0;0 0 1];
     R{3} = [0 -1 0;1 0 0;0 0 1];
     R{4} = [0 1 0;-1 0 0;0 0 1];
-    R{5} = -R{1};
-    R{6} = -R{2};
-    R{7} = -R{3};
-    R{8} = -R{4};
+%     R{5} = -R{1};
+%     R{6} = -R{2};
+%     R{7} = -R{3};
+%     R{8} = -R{4};
 elseif symID==42    %4/mmm
     R{1} = eye(3);
     R{2} = [-1 0 0;0 -1 0;0 0 1];
@@ -54,21 +57,21 @@ elseif symID==42    %4/mmm
     R{6} = [1 0 0;0 -1 0;0 0 -1];
     R{7} = [0 1 0;1 0 0;0 0 -1];
     R{8} = [0 -1 0;-1 0 0;0 0 -1];
-    R{9} = -R{1};
-    R{10} = -R{2};
-    R{11} = -R{3};
-    R{12} = -R{4};
-    R{13} = -R{5};
-    R{14} = -R{6};
-    R{15} = -R{7};
-    R{16} = -R{8};
+%     R{9} = -R{1};
+%     R{10} = -R{2};
+%     R{11} = -R{3};
+%     R{12} = -R{4};
+%     R{13} = -R{5};
+%     R{14} = -R{6};
+%     R{15} = -R{7};
+%     R{16} = -R{8};
 elseif symID==3     % -3
     R{1} = eye(3);
     R{2} = [0 -1 0;1 -1 0;0 0 1];
     R{3} = [-1 1 0;-1 0 0;0 0 1];
-    R{4} = -R{1};
-    R{5} = -R{2};
-    R{6} = -R{3};
+%     R{4} = -R{1};
+%     R{5} = -R{2};
+%     R{6} = -R{3};
 elseif symID==32    % -3m
     R{1} = eye(3);
     R{2} = [0 -1 0;1 -1 0;0 0 1];
@@ -76,12 +79,12 @@ elseif symID==32    % -3m
     R{4} = [0 -1 0;-1 0 0;0 0 -1];
     R{5} = [-1 1 0;0 1 0;0 0 -1];
     R{6} = [1 0 0;1 -1 0;0 0 -1];
-    R{7} = -R{1};
-    R{8} = -R{2};
-    R{9} = -R{3};
-    R{10} = -R{4};
-    R{11} = -R{5};
-    R{12} = -R{6};
+%     R{7} = -R{1};
+%     R{8} = -R{2};
+%     R{9} = -R{3};
+%     R{10} = -R{4};
+%     R{11} = -R{5};
+%     R{12} = -R{6};
 elseif symID==6     % 6/m
     R{1} = eye(3);
     R{2} = [0 -1 0;1 -1 0;0 0 1];
@@ -89,12 +92,12 @@ elseif symID==6     % 6/m
     R{4} = [-1 0 0;0 -1 0;0 0 1];
     R{5} = [0 1 0;-1 1 0;0 0 1];
     R{6} = [1 -1 0;1 0 0;0 0 1];
-    R{7} = -R{1};
-    R{8} = -R{2};
-    R{9} = -R{3};
-    R{10} = -R{4};
-    R{11} = -R{5};
-    R{12} = -R{6};
+%     R{7} = -R{1};
+%     R{8} = -R{2};
+%     R{9} = -R{3};
+%     R{10} = -R{4};
+%     R{11} = -R{5};
+%     R{12} = -R{6};
 elseif symID==62    % 6/mmm
     R{1} = eye(3);
     R{2} = [0 -1 0;1 -1 0;0 0 1];
@@ -108,18 +111,18 @@ elseif symID==62    % 6/mmm
     R{10} = [0 -1 0;-1 0 0;0 0 -1];
     R{11} = [-1 1 0;0 1 0;0 0 -1];
     R{12} = [1 0 0;1 -1 0;0 0 -1];
-    R{13} = -R{1};
-    R{14} = -R{2};
-    R{15} = -R{3};
-    R{16} = -R{4};
-    R{17} = -R{5};
-    R{18} = -R{6};
-    R{19} = -R{7};
-    R{20} = -R{8};
-    R{21} = -R{9};
-    R{22} = -R{10};
-    R{23} = -R{11};
-    R{24} = -R{12};
+%     R{13} = -R{1};
+%     R{14} = -R{2};
+%     R{15} = -R{3};
+%     R{16} = -R{4};
+%     R{17} = -R{5};
+%     R{18} = -R{6};
+%     R{19} = -R{7};
+%     R{20} = -R{8};
+%     R{21} = -R{9};
+%     R{22} = -R{10};
+%     R{23} = -R{11};
+%     R{24} = -R{12};
 elseif symID==23    % m-3
     R{1} = eye(3);
     R{2} = [-1 0 0;0 -1 0;0 0 1];
@@ -133,18 +136,18 @@ elseif symID==23    % m-3
     R{10} = [0 -1 0;0 0 1;-1 0 0];
     R{11} = [0 1 0;0 0 -1;-1 0 0];
     R{12} = [0 -1 0;0 0 -1;1 0 0];
-    R{13} = -R{1};
-    R{14} = -R{2};
-    R{15} = -R{3};
-    R{16} = -R{4};
-    R{17} = -R{5};
-    R{18} = -R{6};
-    R{19} = -R{7};
-    R{20} = -R{8};
-    R{21} = -R{9};
-    R{22} = -R{10};
-    R{23} = -R{11};
-    R{24} = -R{12};
+%     R{13} = -R{1};
+%     R{14} = -R{2};
+%     R{15} = -R{3};
+%     R{16} = -R{4};
+%     R{17} = -R{5};
+%     R{18} = -R{6};
+%     R{19} = -R{7};
+%     R{20} = -R{8};
+%     R{21} = -R{9};
+%     R{22} = -R{10};
+%     R{23} = -R{11};
+%     R{24} = -R{12};
 elseif symID==43    % m-3m
     R{1} = eye(3);
     R{2} = [-1 0 0;0 -1 0;0 0 1];
@@ -170,33 +173,39 @@ elseif symID==43    % m-3m
     R{22} = [0 0 1;0 -1 0;1 0 0];
     R{23} = [0 0 -1;0 1 0;1 0 0];
     R{24} = [0 0 -1;0 -1 0;-1 0 0];
-    R{25} = -R{1};
-    R{26} = -R{2};
-    R{27} = -R{3};
-    R{28} = -R{4};
-    R{29} = -R{5};
-    R{30} = -R{6};
-    R{31} = -R{7};
-    R{32} = -R{8};
-    R{33} = -R{9};
-    R{34} = -R{10};
-    R{35} = -R{11};
-    R{36} = -R{12};
-    R{37} = -R{13};
-    R{38} = -R{14};
-    R{39} = -R{15};
-    R{40} = -R{16};
-    R{41} = -R{17};
-    R{42} = -R{18};
-    R{43} = -R{19};
-    R{44} = -R{20};
-    R{45} = -R{21};
-    R{46} = -R{22};
-    R{47} = -R{23};
-    R{48} = -R{24};
+%     R{25} = -R{1};
+%     R{26} = -R{2};
+%     R{27} = -R{3};
+%     R{28} = -R{4};
+%     R{29} = -R{5};
+%     R{30} = -R{6};
+%     R{31} = -R{7};
+%     R{32} = -R{8};
+%     R{33} = -R{9};
+%     R{34} = -R{10};
+%     R{35} = -R{11};
+%     R{36} = -R{12};
+%     R{37} = -R{13};
+%     R{38} = -R{14};
+%     R{39} = -R{15};
+%     R{40} = -R{16};
+%     R{41} = -R{17};
+%     R{42} = -R{18};
+%     R{43} = -R{19};
+%     R{44} = -R{20};
+%     R{45} = -R{21};
+%     R{46} = -R{22};
+%     R{47} = -R{23};
+%     R{48} = -R{24};
 else
     error('symID not recognized');
 end
+
+
+
+%%% make sure euler angles are double for calcs
+euler1 = double(euler1);
+euler2 = double(euler2);
 
 
 
@@ -226,20 +235,24 @@ misorient = min(misorient,360-misorient);   % put in range 0..180
 
 %%% disorientation
 disorient = misorient;  % initialize, stores current lowest disorientation as it loops through symmetry elements
-R1 = eu2om(euler1*pi/180);  % rotation matrix of euler1
-R2 = eu2om(euler2*pi/180);  % rotation matrix of euler2
 
-% loop through symmetry elements, acting on euler2
-for ii=2:length(R)
-    Rdiff = R1\R{ii}*R2;
-    disorient_temp = acosd((trace(Rdiff)-1)/2);
-    disorient_temp = min(disorient_temp,360-disorient_temp);   % put in range 0..180
-    
-    if disorient_temp < disorient
-        disorient = disorient_temp;     % update if lower than previous value
+if length(R)>1
+    R1 = eu2om(euler1*pi/180);  % rotation matrix of euler1
+    R2 = eu2om(euler2*pi/180);  % rotation matrix of euler2
+
+    % loop through symmetry elements, acting on euler2
+    for ii=2:length(R)
+        Rdiff = R1\R{ii}*R2;
+        disorient_temp = acosd((trace(Rdiff)-1)/2);
+        disorient_temp = min(disorient_temp,360-disorient_temp);   % put in range 0..180
+
+        if disorient_temp < disorient
+            disorient = disorient_temp;     % update if lower than previous value
+        end
     end
+else    % triclinic symmetry
+    % don't need to do anything, disorient already set to misorient
 end
-
 
 
 
